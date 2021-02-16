933c1d84982f5c8e10e0c437ab6be9f41409fe48
PINS Team <copybara-servicebot@google.com>
2021-02-16 14:50:33 -0800

    Added gNOI system validator to pins backend.
    PiperOrigin-RevId: 357809985

--- lib/validator/pins_backend.cc
+++ lib/validator/pins_backend.cc
@@ -37,6 +37,7 @@
 #include "p4_pdpi/connection_management.h"
 #include "proto/gnmi/gnmi.grpc.pb.h"
 #include "proto/gnmi/gnmi.pb.h"
+#include "system/system.pb.h"
 #include "thinkit/switch.h"
 
 namespace pins_test {
@@ -82,6 +83,25 @@ absl::Status PINSBackend::CanGetAllInterfaceOverGnmi(absl::string_view chassis,
   context.set_deadline(absl::ToChronoTime(absl::Now() + timeout));
   context.set_wait_for_ready(true);
   return gutil::GrpcStatusToAbslStatus(gnmi_stub->Get(&context, req, &resp));
+}
+
+absl::Status PINSBackend::CanGetTimeOverGnoiSystem(absl::string_view chassis,
+                                                   absl::Duration timeout) {
+  auto sut = switches_map_.find(chassis);
+  if (sut == switches_map_.end()) {
+    return absl::InternalError(
+        absl::StrCat("ValidatorBackend passed invalid chassis: ", chassis));
+  }
+  auto& [sut_name, sut_switch] = *sut;
+  ASSIGN_OR_RETURN(auto gnoi_system_stub, sut_switch->CreateGnoiSystemStub());
+
+  gnoi::system::TimeRequest request;
+  gnoi::system::TimeResponse response;
+  grpc::ClientContext context;
+  context.set_deadline(absl::ToChronoTime(absl::Now() + timeout));
+  context.set_wait_for_ready(true);
+  return gutil::GrpcStatusToAbslStatus(
+      gnoi_system_stub->Time(&context, request, &response));
 }
 
 }  // namespace pins_test
