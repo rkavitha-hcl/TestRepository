5471b0e13211cb4ba0ae2ed0b8de23279c23825a
PINS Team <copybara-servicebot@google.com>
2021-07-29 10:53:07 -0700

    Added example link flap test using generic testbed.
    PiperOrigin-RevId: 387621513

---
+++ tests/gnmi/link_flap_tests.h
@@ -0,0 +1,12 @@
+#ifndef GOOGLE_TESTS_GNMI_LINK_FLAP_TESTS_H_
+#define GOOGLE_TESTS_GNMI_LINK_FLAP_TESTS_H_
+
+#include "thinkit/generic_testbed_fixture.h"
+
+namespace pins_test {
+
+class ExampleTestFixture : public thinkit::GenericTestbedFixture {};
+
+}  // namespace pins_test
+
+#endif  // GOOGLE_TESTS_GNMI_LINK_FLAP_TESTS_H_
