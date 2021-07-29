4561cdf02d1bfc7f4b1c0d85d1c1af4dbadfea6c
jonathan-dilorenzo <dilo@google.com>
2021-07-28 17:11:24 -0700

    [p4 pdpi] Minor changes to improve readability of p4info_union.cc.
    PiperOrigin-RevId: 387472310

--- p4_pdpi/p4info_union.cc
+++ p4_pdpi/p4info_union.cc
@@ -23,41 +23,52 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "absl/strings/string_view.h"
+#include "absl/strings/substitute.h"
 #include "gutil/proto.h"
 #include "p4/config/v1/p4info.pb.h"
 #include "p4_pdpi/p4info_union_lib.h"
+
 ABSL_FLAG(std::string, list_of_p4infos, "",
           "Comma-separated list of P4Infos files (required)");
-constexpr char kUsage[] = "--list_of_p4infos=<comma-seperated file list>";
+
 using ::p4::config::v1::P4Info;
-int main(int argc, char **argv) {
+
+constexpr absl::string_view kUsage =
+    "--list_of_p4infos=<comma-separated file list>";
+
+int main(int argc, char** argv) {
   absl::SetProgramUsageMessage(
-      absl::StrJoin({"usage: ", (const char *)argv[0], kUsage}, " "));
+      absl::Substitute("usage: $0 $1", argv[0], kUsage));
   absl::ParseCommandLine(argc, argv);
+
   // Get p4info file name.
   if (absl::GetFlag(FLAGS_list_of_p4infos).empty()) {
     std::cerr
-        << "Missing argument: --list_of_p4infos=<comma-seperated file list>"
+        << "Missing argument: --list_of_p4infos=<comma-separated file list>"
         << std::endl;
     return 1;
   }
-
   std::vector<std::string> p4info_files =
       absl::StrSplit(absl::GetFlag(FLAGS_list_of_p4infos), ',');
+
+  // Generate p4infos from file names.
   std::vector<P4Info> p4infos;
-  for (const std::string &p4info_file : p4info_files) {
+  for (const auto& p4info_file : p4info_files) {
     absl::Status status =
         gutil::ReadProtoFromFile(p4info_file, &p4infos.emplace_back());
     if (!status.ok()) {
-      std::cerr << "Failed to parse p4info_file: '" << p4info_file << "'."
-                << " Status: " << status << std::endl;
+      std::cerr << absl::Substitute(
+          "Failed to parse p4info_file: '$0'. Status: $1\n", p4info_file,
+          status.message());
       return 1;
     }
   }
+
   auto p4info_union = pdpi::UnionP4info(p4infos);
   if (!p4info_union.ok()) {
-    std::cerr << "Failed to union p4info files:'" << p4info_union.status()
-              << "'" << std::endl;
+    std::cerr << absl::Substitute("Failed to union p4info files:'$0'\n",
+                                  p4info_union.status().message());
     return 1;
   }
   std::cout << p4info_union->DebugString();
