28f399ac29503ec5975911be3f62874ec4a47106
jonathan-dilorenzo <dilo@google.com>
2021-08-05 13:45:12 -0700

    [P4 Fuzzer] Move tests into pins_infra/tests/forwarding.
    PiperOrigin-RevId: 389014291

--- tests/forwarding/fuzzer_tests.cc
+++ tests/forwarding/fuzzer_tests.cc
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "p4_fuzzer/fuzzer_tests.h"
+#include "tests/forwarding/fuzzer_tests.h"
 
 #include <algorithm>
 #include <set>
