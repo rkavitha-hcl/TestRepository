9ae57d639da4a816ffa2e4c0c0b392eceabe1c0b
PINS Team <copybara-servicebot@google.com>
2021-09-21 15:44:32 -0700

    Enable Sflow test.
    PiperOrigin-RevId: 398102144

---
+++ tests/sflow/sflow_test.h
@@ -0,0 +1,15 @@
+#ifndef GOOGLE_TESTS_SFLOW_SFLOW_TEST_H_
+#define GOOGLE_TESTS_SFLOW_SFLOW_TEST_H_
+
+#include "thinkit/mirror_testbed_fixture.h"
+
+namespace gpins {
+
+// TODO: to be implemented
+class SflowTestFixture : public thinkit::MirrorTestbedFixture {
+ protected:
+};
+
+}  // namespace gpins
+
+#endif  // GOOGLE_TESTS_SFLOW_SFLOW_TEST_H_
