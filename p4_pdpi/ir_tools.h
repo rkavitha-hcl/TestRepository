455ed045f7f0899417c757508661a48f9e6c1adf
jonathan-dilorenzo <dilo@google.com>
2022-02-25 16:32:04 -0800

    [PDPI] Create generic function for manipulating P4 named types.
    PiperOrigin-RevId: 431055731

---
+++ p4_pdpi/ir_tools.h
@@ -0,0 +1,48 @@
+#ifndef GOOGLE_P4_PDPI_IR_TOOLS_H_
+#define GOOGLE_P4_PDPI_IR_TOOLS_H_
+
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/variant.h"
+#include "p4/config/v1/p4types.pb.h"
+#include "p4_pdpi/ir.pb.h"
+namespace pdpi {
+
+// `TransformValuesOfType` uses `info` to find all values of `target_type`
+// in `entry` and apply `transformer` to them.
+//
+// This function fails if:
+// * the 'transformer' fails.
+// * the `info` and the `entry` are incompatible.
+//
+// Note that the IR does not include enough information about counters, meters,
+// or registers to determine if they use a particular P4NamedType. They are thus
+// currently ignored by this function.
+absl::Status TransformValuesOfType(
+    const IrP4Info& info, const p4::config::v1::P4NamedType& target_type,
+    IrTableEntry& entry,
+    const std::function<absl::StatusOr<std::string>(absl::string_view)>&
+        transformer);
+
+// `TransformValuesOfType` uses `info` to find all values of `target_type`
+// in `entries` and apply `transformer` to them. See the `TransformValuesOfType`
+// which acts on a single entry above for further information.
+// If this function returns an error, a prefix of the `entries` may still be
+// transformed by `transformer`.
+absl::Status TransformValuesOfType(
+    const IrP4Info& info, const p4::config::v1::P4NamedType& target_type,
+    std::vector<IrTableEntry>& entries,
+    const std::function<absl::StatusOr<std::string>(absl::string_view)>&
+        transformer);
+
+// TODO: Add a function for packet in and packet out to callfunction on
+// IrPacketMetadata.
+
+}  // namespace pdpi
+
+#endif  // GOOGLE_P4_PDPI_IR_TOOLS_H_
