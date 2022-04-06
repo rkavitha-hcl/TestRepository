236e8be975c1aa97d0b2f2f49ab71a16b3773f99
jonathan-dilorenzo <dilo@google.com>
2022-04-06 15:29:14 -0700

    [P4 Test Library] Create a function that rewrites table entries to only rely on ports from a given gnmi_config.
    PiperOrigin-RevId: 439950856

--- tests/lib/switch_test_setup_helpers.h
+++ tests/lib/switch_test_setup_helpers.h
@@ -4,10 +4,14 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
+#include "absl/container/btree_set.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "p4/config/v1/p4info.pb.h"
+#include "p4_pdpi/ir.pb.h"
 #include "p4_pdpi/p4_runtime_session.h"
 #include "thinkit/switch.h"
 namespace pins_test {
@@ -42,6 +46,32 @@ ConfigureSwitchPairAndReturnP4RuntimeSessionPair(
     std::optional<std::string> gnmi_config,
     std::optional<p4::config::v1::P4Info> p4info,
     const pdpi::P4RuntimeSessionOptionalArgs& metadata = {});
+
+// Gets a count of which P4runtime ports are used in `entries` and how
+// frequently.
+absl::StatusOr<absl::btree_set<std::string>> GetPortsUsed(
+    const pdpi::IrP4Info& info, std::vector<pdpi::IrTableEntry> entries);
+
+// Rewrites the ports of the given table `entries` to only use the given
+// non-empty `ports`. Uses `info` to determine which values refer to ports.
+// Specifically, for each port `x` in the set of table entries, this
+// function remaps it to f(x) such that:
+// - if `x \in ports` then `f(x) = x`
+// - `x` is remapped to an `f(x) \in ports` (chosen deterministically)
+//   such that `\forall. p1,p2 \in ports. |{f(x) = p1 | x \in
+//   all_ports(entries)}| <= |{f(x) = p2 | x \in all_ports(entries)}| + 1`.
+//
+// This function makes it possible to use a list of pre-existing table entries
+// with any set of ports desired (or configured on the switch).
+absl::Status RewritePortsInTableEntries(
+    const pdpi::IrP4Info& info, absl::Span<const std::string> new_ports,
+    std::vector<pdpi::IrTableEntry>& entries);
+
+// Extracts the available ports from the given `gnmi_config` and rewrites
+// entries as per above.
+absl::Status RewritePortsInTableEntries(
+    const pdpi::IrP4Info& info, absl::string_view gnmi_config,
+    std::vector<pdpi::IrTableEntry>& entries);
 
 }  // namespace pins_test
 
