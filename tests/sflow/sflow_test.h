34cc895f8ed00204891ee7871936254dcae9fc67
PINS Team <copybara-servicebot@google.com>
2022-06-21 15:44:39 -0700

    Sflow test basic SetUp
    PiperOrigin-RevId: 456368346

--- tests/sflow/sflow_test.h
+++ tests/sflow/sflow_test.h
@@ -1,13 +1,52 @@
 #ifndef GOOGLE_TESTS_SFLOW_SFLOW_TEST_H_
 #define GOOGLE_TESTS_SFLOW_SFLOW_TEST_H_
+#include <memory>
+#include <string>
+#include <thread>  // NOLINT: Need threads (instead of fiber) for upstream code.
+#include <vector>
 
-#include "thinkit/mirror_testbed_fixture.h"
+#include "absl/memory/memory.h"
+#include "gtest/gtest.h"
+#include "p4_pdpi/ir.h"
+#include "p4_pdpi/p4_runtime_session.h"
+#include "proto/gnmi/gnmi.grpc.pb.h"
+#include "thinkit/generic_testbed_fixture.h"
+#include "thinkit/ssh_client.h"
 
 namespace gpins {
 
-// TODO: to be implemented
-class SflowTestFixture : public thinkit::MirrorTestbedFixture {
+struct SflowTestParams {
+  thinkit::GenericTestbedInterface* testbed_interface;
+  thinkit::SSHClient* ssh_client;
+  std::string gnmi_config;
+  p4::config::v1::P4Info p4_info;
+};
+
+// Structure represents a link between SUT and Ixia.
+// This is represented by Ixia interface name and the SUT's gNMI interface
+// name and its corrosponding p4 runtime id.
+struct IxiaLink {
+  std::string ixia_interface;
+  std::string sut_interface;
+  int port_id;
+};
+
+class SflowTestFixture : public testing::TestWithParam<SflowTestParams> {
  protected:
+  void SetUp() override;
+
+  void TearDown() override;
+
+  const p4::config::v1::P4Info& GetP4Info() { return GetParam().p4_info; }
+  const pdpi::IrP4Info& GetIrP4Info() { return ir_p4_info_; }
+
+  std::unique_ptr<thinkit::GenericTestbed> testbed_;
+  pdpi::IrP4Info ir_p4_info_;
+  std::unique_ptr<gnmi::gNMI::StubInterface> gnmi_stub_;
+  std::unique_ptr<pdpi::P4RuntimeSession> sut_p4_session_;
+  thinkit::SSHClient* ssh_client_ = GetParam().ssh_client;
+
+  std::vector<IxiaLink> ready_links_;
 };
 
 }  // namespace gpins
