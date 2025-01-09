0b7ba6a4862b424870ccd1e629c45ad4c7613a6c
VSuryaprasad-HCL <v.suryaprasad@hcl.com>
2025-01-09 11:34:26 +0000

    Removing the unwatned changes

--- sai_p4/instantiations/google/roles.h
+++
@@ -1,7 +0,0 @@
-#ifndef PINS_SAI_ROLES_P4_
-#define PINS_SAI_ROLES_P4_
-
-// We don't use instantion-specific roles at the moment.
-// See the same file under the "fixed" directory for additional roles.
-
-#endif // PINS_SAI_ROLES_P4_
